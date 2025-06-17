c81afeaf6aaf8ec7613523c9b808c8464a7ddcb9
RohanSingh <Rohan.singh04@outlook.com>
2025-06-17 19:15:36 -0400

    Initial Commit

---
+++ Health Tracker/Health_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_TrackerApp.swift
+//  Health Tracker
+//
+//  Created by Rohan Singh on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Health_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Health TrackerTests/Health_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Health_TrackerTests.swift
+//  Health TrackerTests
+//
+//  Created by Rohan Singh on 6/17/25.
+//
+
+import Testing
+@testable import Health_Tracker
+
+struct Health_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
